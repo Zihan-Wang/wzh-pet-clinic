36a9e31288bb04b6f0a8c773402eafebe645b6c6
Zihan-Wang <zihanwangbuaa@outlook.com>
2019-09-25 10:30:49 -0700

    create vet index and controller

---
+++ pet-clinic-data/src/main/java/guru/springframework/wzhpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,45 @@
+package guru.springframework.wzhpetclinic.services.map;
+
+import java.util.Set;
+
+import guru.springframework.wzhpetclinic.model.Owner;
+import guru.springframework.wzhpetclinic.services.CrudService;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+	@Override
+	public Owner save(Owner object) {
+		// TODO Auto-generated method stub
+		return super.save(object.getId(), object);
+	}
+
+	@Override
+	public
+	Set<Owner> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public
+	Owner findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public
+	void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public
+	void delete(Owner object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+	
+
+}
